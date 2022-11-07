78e8cabf2b006f998bc1f6235ba774d97cfef87e
Rodrigue CODO <codorol22@gmail.com>
2022-11-07 19:48:14 +0500

    Article content fetching given an url

--- articles.py
+++ articles.py
@@ -2,9 +2,19 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
-source_base = 'https://theconversation.com/us/'
-source_url = 'https://theconversation.com/us/technology'
-source_html = requests.get(source_url).text
+source_base = 'https://theconversation.com/us'
+
+source_article_url = 'https://theconversation.com/humans-are-8-virus-how-the-ancient-viral-dna-in-your-genome-plays-a-role-in-human-disease-and-development-192322'
+
+def get_article_content(article_url):
+    article_page = requests.get(article_url).text
+    article_soup = BeautifulSoup(article_page, 'lxml')
+    body = article_soup.find('div', itemprop = 'articleBody')
+    return body.get_text().strip()
+
+
+source_main_page_url = 'https://theconversation.com/us/technology'
+source_html = requests.get(source_main_page_url).text
 soup = BeautifulSoup(source_html, 'lxml')
 articles_dict = {}
 articles = soup.find_all('article')
@@ -23,17 +33,20 @@ for count,article in enumerate(articles):
     if article_header:
         link = article_header.a
         title = link.get_text()
-        link = link.get('href')
+        articles_dict[count]['title'] = title
+
+        relative_link = link.get('href')
+        # content = body.get_text()
+        # articles_dict[count]['content'] = content
+
         authors = []
         if article.p :
             for a in article.p.find_all('a'):
                 authors.append(a.get_text())
-        articles_dict[count]['title'] = title
-        articles_dict[count]['link'] = link
         articles_dict[count]['authors'] = authors 
     summary = article.find('div', class_ = 'content')
     if summary:
         articles_dict[count]['summary'] = summary.span.get_text()
 
 articles_json = json.dumps(articles_dict, indent = 4)
-print(articles_json)
+# print(articles_json)
